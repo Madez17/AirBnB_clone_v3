2d6ed4f7c393df1b8c18343f890ea851826c2e5b
naxus1 <naxus1@gmail.com>
2019-09-11 23:19:07 +0000

    Create  method app.py

---
+++ api/v1/views/states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""comment of class"""
+
+from api.v1.views import app_views
+import models
+from flask import Flask, jsonify, request, abort
+
+
+@app_views.route("/states", strict_slashes=False, methods=['GET'])
+def list_states_json():
+    list_to_json = []
+    all_objects = models.storage.all("State")
+
+    for key, value in all_objects.items():
+        list_to_json.append(value.to_dict())
+    return (jsonify(list_to_json))
+
+
+@app_views.route("/states/<state_id>", strict_slashes=False, methods=['GET'])
+def list_states_id(state_id):
+    var = models.storage.get("State", state_id)
+    if var is None:
+        abort(404)
+    else:
+        objet_list = var.to_dict()
+        return(jsonify(objet_list))
+
+
+@app_views.route("/states/<state_id>", strict_slashes=False, methods=['DELETE'])
+def list_states_delete(state_id):
+    var = models.storage.get("State", state_id)
+    if var is None:
+        abort(404)
+    else:
+        models.storage.delete(var)
+        models.storage.save()
+        dict_state = []
+        return (jsonify(dict_state))
