2a15471b35f1852e4a1f374c37774f656e9acde5
Madez <781@holbertonschool.com>
2019-09-12 23:33:58 +0000

    [Fixed] - variable

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,19 +9,20 @@ from flask import Flask, jsonify, request, abort, make_response
 @app_views.route("/states/<state_id>/cities", strict_slashes=False,
                  methods=['GET'])
 def list_cities_json(state_id):
-    """ Method GET show all Cities"""
+    """ Method GET show all Cities """
     list_city_to_json = []
     filter_states = storage.get("State", state_id)
     if filter_states is None:
         abort(404)
     else:
-        for iter in filter_states.cities:
-            list_city_to_json.append(iter.to_dict())
+        for itera in filter_states.cities:
+            list_city_to_json.append(itera.to_dict())
         return jsonify(list_city_to_json)
 
 
 @app_views.route("/cities/<city_id>", methods=['GET'], strict_slashes=False)
 def list_cities_id(city_id):
+    """ doc """
     filter_id_city = storage.get("City", city_id)
     if filter_id_city is None:
         abort(404)
@@ -31,7 +32,7 @@ def list_cities_id(city_id):
 @app_views.route("/cities/<city_id>",
                  methods=['DELETE'], strict_slashes=False)
 def list_cities_delete(city_id):
-    """Method delete"""
+    """ Method delete """
     filter_id_city = storage.get("City", city_id)
     if filter_id_city is None:
         abort(404)
@@ -45,7 +46,7 @@ def list_cities_delete(city_id):
 @app_views.route("/states/<state_id>/cities", strict_slashes=False,
                  methods=['POST'])
 def post_city(state_id):
-    """Method post"""
+    """ Method post """
     filter_state = storage.get("State", state_id)
     if filter_state is None:
         abort(404)
@@ -64,7 +65,7 @@ def post_city(state_id):
 
 @app_views.route("/cities/<city_id>", strict_slashes=False, methods=['PUT'])
 def put_cities(city_id):
-    """Method put"""
+    """ Method put """
     filter_id_city = storage.get("City", city_id)
     restrictions = ["id", "update_at", "create_at", "state_id"]
     if filter_id_city is None:
